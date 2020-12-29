a40dd7e5395392c9d1cbe0b986d07679ba779b84
EC2 Default User <ec2-user@ip-172-31-74-237.ec2.internal>
2020-12-29 14:47:13 +0000

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -23,10 +23,15 @@ root 'static_pages#home'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 
 
 end
